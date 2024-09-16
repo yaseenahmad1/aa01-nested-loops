7127f8f5ffcdb102df0f0cec032333bda1e74f93
yaseenahmad1 <yahmad09@gmail.com>
2024-09-15 18:47:33 -0700

    problem 3 solved

--- problems/03-two-dimensional-product.js
+++ problems/03-two-dimensional-product.js
@@ -4,10 +4,23 @@ numbers as an argument. The function should return the total product of all
 numbers multiplied together.
 */
 
-// Your code here 
+// just like the previous example except this time we will be multiplying. 
+function twoDimensionalProduct(array) {
+    let totalProduct = 1; //my logic is, unlike the let totalSum = 0; example from the previous problem, if we multiply our elements with 0 we will get zero and not the total of the product so we will start at 1 to ensure the numbers exceed zero.
 
-// let arr1 = [[6, 4], [5], [3, 1]];
-// console.log(twoDimensionalProduct(arr1)); // 360
+    for (i = 0; i < array.length; i++) {//here we use i++ because we are trying to figure out how many subarrays we have within our array. We are not multiplying them because we have to mulitply the elements within them first. 
+        let subArr = array[i]; // we have assigned each subarray index to the variable 'subArr'
+        for (j = 0; j < subArr.length; j++) { //not sure what dictation to use to multiply my j indices together.
+            let element = subArr[j]; //store the product of j within the element variable. 
+            totalProduct *= element; //this should start to multiply each element within each subarray together. 
+        }
+
+    }
+    return totalProduct; //this should return the final product of the argument given. 
+}
+
+let arr1 = [[6, 4], [5], [3, 1]];
+console.log(twoDimensionalProduct(arr1)); // 360
 
 // let arr2 = [[11, 4], [2]];
 // console.log(twoDimensionalProduct(arr2)); // 88
