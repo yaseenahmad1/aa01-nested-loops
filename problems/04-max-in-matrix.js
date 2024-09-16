6814cb58dfe4012c2c4e053b42aa65cf03d05279
yaseenahmad1 <yahmad09@gmail.com>
2024-09-15 21:18:59 -0700

    problem 4 solved

--- problems/04-max-in-matrix.js
+++ problems/04-max-in-matrix.js
@@ -5,16 +5,26 @@ least one value.
 */
 
 function maxInMatrix(matrix) {
-  // Your code here 
+    let max = (matrix[0][0]);//this sets up the first bracket as the first subarray and the second bracket as the first index within that array. 
+
+    for (i = 0; i < matrix.length; i++) { //this loop goes through the subarrays within our 2d array.
+        let subArr = matrix[i];
+        for (j = 0; j < subArr.length; j++) { //this loop goes through each element within each subarray. 
+            if (matrix[i][j] > max) { //after each iteration, this 'if' statement ensures that if the subarray [i] and the element existing within that subarray [j] is greater than the very first element within the very first subarray...
+                max = matrix[i][j]; //then to replace that variable 'max' with the greater matrix[i][j] and to continue this process until the largest value is found. 
+            }
+        }
+    }
+    return max; //this will return the largest value out of all subarray and indices within those subarrays. 
 }
 
-// matrix = [
-//   [11, 2, -99],
-//   [20, 19, 10],
-//   [47, 72, 56],
-// ];
+matrix = [
+  [11, 2, -99],
+  [20, 19, 10],
+  [47, 72, 56],
+];
 
-// console.log(maxInMatrix(matrix)); // 72
+console.log(maxInMatrix(matrix)); // 72
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
 module.exports = maxInMatrix;
